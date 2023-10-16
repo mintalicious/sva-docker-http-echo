d5e4e609cab89fd13b24bd3d8fd7951124865dad
Erik Schindler <mintalicious@web4ward.de>
2023-10-16 16:35:48 +0200

    update build and publish scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t mintalicious/sva-http-echo:8-aap -t mintalicious/sva-http-echo:latest .
+docker build -t mintalicious/sva-docker-http-echo:8-aap -t mintalicious/sva-docker-http-echo:latest .
